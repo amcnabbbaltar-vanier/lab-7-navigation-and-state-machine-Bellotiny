dee7aeb52c1315bd76fc4918ac1a33182e3413e0
Sen B. Donissaint <donissaint13@gmail.com>
2025-03-26 15:09:37 -0400

    Finished
    Done

--- Assets/Scripts/State/PatrolState.cs
+++ Assets/Scripts/State/PatrolState.cs
@@ -1,4 +1,40 @@
 public class PatrolState : IState
 {
-   
+   private AIController aiController;
+   private int currentWayPointIndex = 0;
+
+   public StateType Type => StateType.Patrol;
+
+   public PatrolState(AIController aiController){
+        this.aiController = aiController;
+   }
+
+   public void Enter(){
+        //aiController.Animator.SetBool("isMoving", true);
+        MoveToNextWaypoint();
+   }
+
+   public void Execute(){
+        if (aiController.CanSeePlayer()){
+            aiController.StateMachine.TransitionToState(StateType.Chase);
+            return;
+        }
+        if (!aiController.Agent.pathPending &&
+            aiController.Agent.remainingDistance <= aiController.Agent.stoppingDistance)
+        {
+            MoveToNextWaypoint();
+        }
+    }
+
+    public void Exit(){
+        //Cleanup
+    }
+
+    private void MoveToNextWaypoint(){
+        if(aiController.Waypoints.Length == 0){
+            return;
+        }
+        aiController.Agent.destination = aiController.Waypoints[currentWayPointIndex].position;
+        currentWayPointIndex = (currentWayPointIndex + 1) % aiController.Waypoints.Length;
+    }
 }
